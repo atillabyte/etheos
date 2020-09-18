32e9b73768dd2bcb9a4816f407c8f06864a53850
Ethan Moffat <ethanmoffat@hotmail.com>
2020-09-17 21:11:05 -0700

    Rename hashupdater file -> loginmanager

--- cmake/SourceFileList.cmake
+++ cmake/SourceFileList.cmake
@@ -131,10 +131,10 @@ set(eoserv_ALL_SOURCE_FILES
 	src/handlers/Welcome.cpp
 	src/hash.cpp
 	src/hash.hpp
-	src/hashupdater.cpp
-	src/hashupdater.hpp
 	src/i18n.cpp
 	src/i18n.hpp
+	src/loginmanager.cpp
+	src/loginmanager.hpp
 	src/main.cpp
 	src/map.cpp
 	src/map.hpp
